5cabdfc0a9c0a9c25066e0e32bed3996fe0877a1
Marko <markogidej@yahoo.com>
2024-07-12 17:37:40 -0400

    Loading character list, Q1T2 done

--- marvel-react-project/src/components/CharacterList.jsx
+++ marvel-react-project/src/components/CharacterList.jsx
@@ -5,16 +5,15 @@ const CharacterList = () => {
     const publicKey = '10e6807befd8aeb6f15c4e5356806e98';
     const hashKey = '74068a949a4478d79f2be1e5c96470be';
 
-    const [characterList, setChartacterList] = useState([]);
-
+    const [characterList, setChartacterList] = useState([])
+    
     useEffect(() => {
         async function fetchCharacterList() {
             try {                
                 const response = await axios.get(`https://gateway.marvel.com/v1/public/characters?ts=1&apikey=${publicKey}&hash=${hashKey}`)
                 setChartacterList(response.data.data.results);
-                console.log("api call");
             } catch (error) {
-                console.error("Error fetchign characters list:", error);
+                console.error("Error fetching characters list:", error);
             }
         }
 
@@ -22,15 +21,15 @@ const CharacterList = () => {
         console.log("use effect");
     }, [])
 
-    console.log(characterList);
     return (
-        <div style={{display: "flex"}}>
-            {characterList &&
-            characterList.map((character) => {
-                <div>
-                    <img src={character.thumbnail.path + character.thumbnail.extension} alt="" />
-                    <h3>{character.name}</h3>
-                </div>
+        <div>
+            {characterList.map((character, index) => {
+                return(
+                    <div key={index}>
+                        <img style={{maxWidth: "150px"}} src={character.thumbnail.path + "." + character.thumbnail.extension} alt="" />
+                        <h3>{character.name}</h3>
+                    </div>
+                )
             })}
         </div>
     )
